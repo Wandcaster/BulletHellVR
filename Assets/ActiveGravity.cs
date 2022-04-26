d26267a97c2a2f7541fa7172d9f2dc6a1f9fb1fc
Wandcaster <wiktordojlida@gmail.com>
2022-04-26 21:45:10 +0200

    Przejście na URP Ręce oraz kij do odbijania

---
+++ Assets/ActiveGravity.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ActiveGravity : MonoBehaviour
+{
+    private void Start()
+    {
+        //dOKOÑCZYÆ
+        if(Input.GetKeyDown(KeyCode.Space))
+        Player.instance.hands[0].AttachObject(gameObject, GrabTypes.Grip);
+
+    }
+
+    [SerializeField]
+    float ForceMultiplier;
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            collision.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(collision.gameObject.GetComponent<Rigidbody>().velocity * ForceMultiplier);
+        }
+    }
+}
--- Assets/WIktor/FollowObject.cs
+++ Assets/WIktor/FollowObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class FollowObject : MonoBehaviour
 {
@@ -9,12 +10,46 @@ public class FollowObject : MonoBehaviour
     [SerializeField]
     private Vector3 Offset;
     [SerializeField]
-    private Vector3 OffsetRotation;
+    bool LeftHand;
+    [SerializeField]
+    bool RightHand;
 
+    private void Start()
+    {
+        if (LeftHand) StartCoroutine(GetLeftHand());
+        if (RightHand) StartCoroutine(GetRightHand());
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.position+ Offset;
-        transform.localRotation = Quaternion.Euler(target.rotation.eulerAngles+OffsetRotation);
+        transform.position = target.position + Offset;
+        //transform.localRotation = Quaternion.Euler(target.rotation.eulerAngles+OffsetRotation);
+    }
+    IEnumerator GetLeftHand()
+    {
+        Transform hand = null;
+        while (hand == null)
+        {
+            yield return new WaitForSeconds(1f);
+            if (Player.instance.hands[0].HasSkeleton())
+            {
+                hand = target.Find("LeftRenderModel Slim(Clone)").Find("vr_glove_left_model_slim(Clone)").Find("slim_l").Find("Root").Find("wrist_r");
+            }
+            }
+            target = hand;
+    }
+    IEnumerator GetRightHand()
+    {
+        Transform hand = null;
+        while (hand == null)
+        {
+            yield return new WaitForSeconds(1f);
+            if (Player.instance.hands[1].HasSkeleton())
+            {
+                hand = target.Find("RightRenderModel Slim(Clone)").Find("vr_glove_right_model_slim(Clone)").Find("slim_r").Find("Root").Find("wrist_r");
+            }
+        }
+        target = hand;
     }
 }
+
